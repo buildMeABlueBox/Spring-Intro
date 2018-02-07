3ffb83af1b273c6f99731dadcf2d4ce8e6f1c480
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-02-06 20:20:53 -0500

    added repository interface for users

---
+++ src/com.ajk/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ajk.repositories;
+
+import com.ajk.model.Users;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<Users, String>{
+
+}
