8f88bc6c924bbc0752f2d2e47d6f13e11109f411
Abhijit Kamat <avk41+blah@scarletmail.rutgers.edu>
2018-02-06 20:52:17 -0500

    intellij naming convention issue

--- src/com.ajk/repositories/UsersRepository.java
+++ src/com.ajk/repositories/UsersRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<Users, String>{
+public interface UsersRepository extends MongoRepository<Users, String>{
 
 }
